13e361740f605c5bbcdad89f2c41af3ba2f955c2
zackel <legris.thomas@gmail.com>
2015-03-18 00:18:35 +0100

    Remove more links

--- export.sh
+++ export.sh
@@ -3,6 +3,10 @@ DIR=$(cd $(dirname "$0"); pwd)
 cd $DIR 1>/dev/null
 find ~ -maxdepth 1 -name ".vim*" -type l -exec rm -v {} \;
 find ~ -maxdepth 1 -name ".zsh*" -type l -exec rm -v {} \;
+find ~ -maxdepth 1 -name ".tmux*" -type l -exec rm -v {} \;
+find ~ -maxdepth 1 -name ".gdb*" -type l -exec rm -v {} \;
+find ~ -maxdepth 1 -name ".colorgcc*" -type l -exec rm -v {} \;
+find ~ -maxdepth 1 -name ".screen*" -type l -exec rm -v {} \;
 
 ln -s $PWD/.vim ~
 ln -s $PWD/.vim/.vimrc ~
