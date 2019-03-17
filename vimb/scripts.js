db9d74c6ff5d38811ebf92086e1d62e96420be93
Thomas Legris <legris.thomas@gmail.com>
2019-03-17 23:34:37 +0000

    Add terminus-like font to vimb rendering

--- vimb/scripts.js
+++ vimb/scripts.js
@@ -1,5 +1,18 @@
 'use strict';
 
+var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://fonts.googleapis.com/css?family=Share Tech Mono';
+    link.media = 'all';
+    head.appendChild(link);
+}
+
 function github_wc(repo)
 {
     if(typeof repo === "undefined") {
